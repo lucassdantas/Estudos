f8f1c52aa3f2047fece3e3206648118058d7532c
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-01-31 20:36:43 -0300

    JAVA POO

---
+++ Conceitos/JAVA POO/Caneta.java
@@ -0,0 +1,91 @@
+import java.lang.invoke.SwitchPoint;
+import java.net.SocketPermission;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class ContaBanco {
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+    public void abrirConta() {
+        this.setStatus(true);
+    }
+    public void fecharConta() {
+        this.setStatus(false);
+    }
+    public void depositar(float depValue) {
+        if (this.status){
+            this.setSaldo(this.getSaldo() + depValue);   
+        } else { System.out.println("quer depositar com a conta fechada? hihi");
+            }
+    }
+    public void sacar(float sacValue) {
+        this.setSaldo(this.getSaldo() - sacValue);
+    }
+    public void mensalidade() {
+        this.setSaldo(this.getSaldo() - 12);
+    }
+    public ContaBanco () {
+        this.status = true;
+        switch (this.tipo) {
+            case "CC":
+                this.saldo = 50;
+                break;
+            case "CP":
+                this.saldo = 150;
+                break;
+                
+        
+            default:
+                break;
+        }
+        
+    }
+
+    public void setNumAccount (int n) {
+        this.numConta = n;
+    }
+    public int getNumAccount (){
+        return this.numConta;
+    }
+    public void setTipo(String x){
+        switch (x) {
+            case "CC":
+            this.tipo = x; 
+                break;
+            case "CP":
+            this.tipo = x; 
+                break;
+        
+            default:
+            System.out.println("insira um tipo válido");
+                break;
+        }
+    }
+    public String getTipo() {
+        return this.tipo;
+    }
+    public void setDono (String x) {
+        this.dono = x;
+    }
+    public String getDono () {
+        return this.dono;
+    }
+
+    public void setSaldo (float depValue) {
+        this.saldo = depValue;
+    }
+    public float getSaldo () {
+        return this.saldo;
+    }
+
+    public void setStatus (boolean x){
+        this.status = x;
+    }
+    public String getStatus () {
+        return this.status? "Conta aberta":"Conta fechada";
+    }
+
+}   
---
+++ Conceitos/JAVA POO/arquivo2.java
@@ -0,0 +1,5 @@
+public class arquivo2 {
+    public static void main(String[] args) {
+
+    }
+}
