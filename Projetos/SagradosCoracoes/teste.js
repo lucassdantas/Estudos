40e2ed43ed6df79cf954990363125832ea7de9b7
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-02-09 06:43:32 -0300

    Some studys and projects

---
+++ Projetos/SagradosCoracoes/teste.js
@@ -0,0 +1,20 @@
+
+let oi = new Object()
+
+console.log(oi)
+
+switch(2+2){
+  case 1+1+1+1:
+    console.log("arroz");
+    break;
+}
+let a = [
+  1, 2, 3
+]
+let batata = "oiie"
+let item = a
+for (let i = 0, item= a.length; i < item; i++){
+  console.log(i)
+  "oi"
+  console.log(batata)
+}
