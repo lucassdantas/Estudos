7d8bc262817dffba573c7c62eb59f6bbaa8fa6f2
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-02-09 22:08:41 -0300

    Respiraçao lirica e alerações no sagrado coração

---
+++ Projetos/Exercicio respiratorio app/script.js
@@ -0,0 +1,22 @@
+let ExpiracaoV = 2 //document.querySelector("")
+let Repeticao = [2] /*document.querySelector("")*/
+let Inspiracao = [5]
+let Pause = [5]
+let Expiracao = [ExpiracaoV + Pause]
+
+
+let Contador = function (Value) {
+        let Count = Value[0]    
+        for (i = 0; i <= Count; i++){
+            Value[i] = i
+        }     
+        return Value   
+}
+
+let x = [5]
+console.log(Contador(x))
+
+for (let i = 0; i <= Repeticao; i++) {
+    
+
+    }
