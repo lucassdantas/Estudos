64a0f973de4493475e666ad925b82122b5b22f39
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-03-11 17:56:31 -0300

    public, private, extend, const

--- Conceitos/PHP POO/classes.php
+++ Conceitos/PHP POO/classes.php
@@ -9,6 +9,7 @@
 <body>
     <?php
     class Cristo{
+        const ser = "Deus";
         public $nome;
         
         public function __construct($nome){
@@ -18,6 +19,10 @@
             echo "Eu, ".$this->nome." te batizo em nome do Pai, do Filho e do Espirito Santo. Amem";
         }
 
+        public function Perdoar(){
+            echo "Eu te absolvo de teus pecados em nome do Pai, do Filho e do Espirito Santo. Amém";
+
+        }
         public function getNome(){
             return $this->nome;
         }
@@ -27,10 +32,24 @@
         }
         
     }
-    $padre = new Cristo("Pio");
-
+    #para exibir a constante,
+    #self refere-se ao elemento
+    #dentro do escopo.
+    #parent refere-se ao
+    #elemento que está
+    #no escopo pai
+    class Padre extends Cristo{
+        public $nome;
+        public function Absolver(){
+            echo "Eu, em nome de Jesus que é ". self::ser." te batizo em nome do Pai, do Filho, e do Espirito Santo. Amém";
+        }
+    }
+    $padre = new Padre("Pio");
+    $padre->Perdoar();
+    echo "<br>";
+    $padre->Absolver();
     echo $padre->getNome();
-    
+
     ?>
     <h1> oi</h1>
 </body>
