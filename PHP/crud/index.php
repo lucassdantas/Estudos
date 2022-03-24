36655c12e7331c9c643c4684b6d99f11dd0ff362
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-03-24 20:46:49 -0300

    Add files via upload

--- PHP/crud/index.php
+++ PHP/crud/index.php
@@ -2,7 +2,27 @@
 require_once "./includes/header.php";
 ?>
 
-
+<div class="row">
+    <div class="col s12 m6 push-m3">
+        <h3 class="light">CLIENTES</h3>
+        <Table class="striped">
+            <thead>
+                <tr>
+                    <th>NOME</th>
+                    <th>SOBRENOME</th>
+                    <th>IDADE</th>
+                    <th>EMAIL</th>
+                </tr>
+            </thead>
+            <tbody>
+                <td>LUCAS</td>
+                <td>DANTAS</td>
+                <td>20</td>
+                <td>@gmail.com</td>
+            </tbody>
+        </Table>
+    </div>
+</div>
 
         
 <?php 
