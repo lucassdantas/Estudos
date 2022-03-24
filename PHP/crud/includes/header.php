7575fd065229c5c1c85d56d8186f28132aee6f27
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-03-24 20:34:35 -0300

    Add files via upload

---
+++ PHP/crud/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+        <meta charset="utf-8">
+        <title> SISTEMA DE CADASTRO DE CLIENTS </title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+            <!-- Compiled and minified CSS -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
---
+++ PHP/crud/index.php
@@ -0,0 +1,10 @@
+<?php 
+require_once "./includes/header.php";
+?>
+
+
+
+        
+<?php 
+require_once "./includes/footer.php";
+?>
