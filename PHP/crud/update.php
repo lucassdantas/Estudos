df1b544085ef5a8e697f34dd967cca582250831d
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-04-01 20:38:11 -0300

    Add files via upload

--- PHP/crud/update.php
+++ PHP/crud/update.php
@@ -2,15 +2,17 @@
 session_start();
 require_once "./db_connect.php";
 if(isset($_POST["btn-editar"])){
+    $nome = mysqli_escape_string($connect, $_POST["nome"]);
+    $sobrenome = mysqli_escape_string($connect, $_POST["sobrenome"]);
+    $email = mysqli_escape_string($connect, $_POST["email"]);
+    $idade = mysqli_escape_string($connect, $_POST["idade"]);
+    $id = mysqli_escape_string($connect, $_POST["id"]);
     if((filter_var($email, FILTER_VALIDATE_EMAIL)) && (filter_var($idade, FILTER_VALIDATE_INT)))  {
-        $nome = mysqli_escape_string($connect, $_POST["nome"]);
-        $sobrenome = mysqli_escape_string($connect, $_POST["sobrenome"]);
-        $email = mysqli_escape_string($connect, $_POST["email"]);
-        $idade = mysqli_escape_string($connect, $_POST["idade"]);
-        $id = mysqli_escape_string($connect, $_POST["id"]);
         $sql = "UPDATE clientes SET nome = '$nome', sobrenome = '$sobrenome', email = '$email', idade = '$idade' WHERE id = '$id'";
+        if(mysqli_query($connect, $sql)){
         $_SESSION["mensagem"] = "Atualizado com sucesso";
         header('Location: ./index.php');
+        }
     }
     else{
         $_SESSION["mensagem"] = "Erro ao atualizar";
