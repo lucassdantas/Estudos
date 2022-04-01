9fc37944e201ccb0be33e7fff186ad4712b9933d
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-04-01 20:31:33 -0300

    add edit page with error

--- PHP/crud/editar.php
+++ PHP/crud/editar.php
@@ -1,6 +1,6 @@
 <?php 
-require_once "db_connect.php";
-require_once "includes/header.php";
+require_once "./db_connect.php";
+require_once "./includes/header.php";
 if(isset($_GET["id"])){
     $id = mysqli_escape_string($connect, $_GET["id"]);
     $sql = "SELECT * FROM clientes WHERE id = '$id'";
@@ -12,7 +12,8 @@ if(isset($_GET["id"])){
 <div class="row">
     <div class="col s12 m6 push-m3">
         <h3 class="light">EDITAR CLIENTE</h3>
-        <form action="update.php" method="POST">
+        <form action="./update.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $dados["id"];?>">
             <div class="input-field col s12">
                 <input type="text" name="nome" id="nome" value="<?php echo $dados["nome"];?>">
                 <label for="nome">NOME</label>
@@ -22,18 +23,14 @@ if(isset($_GET["id"])){
                 <label for="sobrenome">SOBRENOME</label>
             </div>
             <div class="input-field col s12">
-                <input type="text" 
-                name="email"
-                id="email" value="<?php echo $dados["email"];?>">
+                <input type="text" name="email" id="email" value="<?php echo $dados["email"];?>">
                 <label for="email">EMAIL</label>
             </div>
             <div class="input-field col s12">
-                <input type="text" 
-                name="idade"
-                id="idade" value="<?php echo $dados["idade"];?>">
+                <input type="text" name="idade" id="idade" value="<?php echo $dados["idade"];?>">
                 <label for="idade">IDADE</label>
             </div>
-            <button type="submit" name="btn-cadastrar" class="btn red">ATUALIZAR</button>
+            <button type="submit" name="btn-editar" class="btn red">ATUALIZAR</button>
             <a href="index.php" type="submit" class="btn green">LISTA DE CLIENTES</a>
         </form>
         
@@ -42,6 +39,6 @@ if(isset($_GET["id"])){
 
         
 <?php 
-require_once "includes/footer.php";
+require_once "./includes/footer.php";
 ?>
 
---
+++ PHP/crud/update.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require_once "./db_connect.php";
+if(isset($_POST["btn-editar"])){
+    if((filter_var($email, FILTER_VALIDATE_EMAIL)) && (filter_var($idade, FILTER_VALIDATE_INT)))  {
+        $nome = mysqli_escape_string($connect, $_POST["nome"]);
+        $sobrenome = mysqli_escape_string($connect, $_POST["sobrenome"]);
+        $email = mysqli_escape_string($connect, $_POST["email"]);
+        $idade = mysqli_escape_string($connect, $_POST["idade"]);
+        $id = mysqli_escape_string($connect, $_POST["id"]);
+        $sql = "UPDATE clientes SET nome = '$nome', sobrenome = '$sobrenome', email = '$email', idade = '$idade' WHERE id = '$id'";
+        $_SESSION["mensagem"] = "Atualizado com sucesso";
+        header('Location: ./index.php');
+    }
+    else{
+        $_SESSION["mensagem"] = "Erro ao atualizar";
+        header('Location: ./index.php');
+    }
+    
+}
+?>
