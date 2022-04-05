55bbdeeb705ca7049d844b8359abc0928cf9b72c
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-04-05 06:49:52 -0300

    Add files via upload

--- PHP/data.php
+++ PHP/data.php
@@ -2,5 +2,9 @@
 #letras d, m e y = numero e ano com 2 digitos
 #letras D, M E Y = dia e mes escrito e ano com 4 digitos
 #l é o dia da semana
-echo date("d");
+#h = hora em formato de 12 h
+#H = HORA EM FORMATO DE 24 H
+# i = minutos
+# s = segundos
+echo date("d/m/Y H:i");
 ?>
