486293a63d25867469d4496826ef063634f7e7fe
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-04-17 12:30:27 -0300

    Add files via upload

---
+++ JavaScript/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name = "lucas"
+  const uperName = name.toUpperCase()
+  function soma(a, b){
+    return a+b
+  }
+  const url = "img/nossa-senhora-das-graças.jpg/150"
+  return (
+    //É NECESSÁRIO USAR "className" para declarar classes pois "class" é
+    //uma palavra reservada do react
+    <div className="App">
+      <header className="App-header">
+        <h1> aprendendo react</h1>
+        <p>aluno: {name}</p>
+        <p>aluno: {uperName+" maiusculo"}</p>
+        <p>nota: {(5+5)+" soma"}</p>
+        <p>função: {soma(1, 2)}</p>
+        <img src={url} alt="o endereço deve ficar entre chaves se as aspas estiverem na varaivel"/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
