26f830e2b7b999ad9435c9a0052b3eec9e6f8a6a
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-16 22:50:32 -0300

    INSET INTO
    inset into table ( aqui coloco os valores dos nomes da coluna caso a ordem do inset into seja diferente da ordem da tabela) 
    values (aqui coloco os valores);

--- MySQL/Estudos.sql
+++ MySQL/Estudos.sql
@@ -1,22 +1,17 @@
-create database cadastro default character set utf8 default collate utf8_general_ci;
-use cadastro;
-CREATE TABLE tabela (
+create database click default character set utf8 default collate utf8_general_ci;
+use click;
+create table funcionarios(
 id int not null auto_increment,
 nome varchar(50) not null,
-nascimento date, 
-sexo enum("F", "M"),
-peso decimal(5,3),
-altura decimal(3,2),
-nacionalidade varchar(20) default "Brasileiro",
-primary key(id)
-) default charset = utf8;
-
-create database clientes02 default character set utf8 default collate utf8_general_ci; 
-create table tabela_cliente(
-id int not null auto_increment,
-nome varchar(30) not null,
-peso decimal(5,2),
 nascimento date,
-velho enum("Y","N"),
+funcao varchar(20),
+sala int(4) default "217",
+primary key(id)
+)default charset utf8;
 
- default charset utf8;
+use click;
+insert into funcionarios
+(id, nome, nascimento, funcao, sala)
+values
+(default, 'lucas', '2002-01-07', 'web developer', '217');
+
