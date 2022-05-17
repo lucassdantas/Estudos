518a9f7bca58dae6411df7e08049b41fe85c4268
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-16 22:22:12 -0300

    create table commands
    create database nome default character set utf8 default collate utf8_general_ci;
    create table nome( 
    data type not null auto increment, 
    primary key(data)) default charset utf8;

--- MySQL/Estudos.sql
+++ MySQL/Estudos.sql
@@ -1,7 +1,22 @@
-CREATE TABLE clientes (
-nome varchar(30),
-sexo char(1),
-peso  float,
-altura float,
-nacionalidade varchar(30)
-);
+create database cadastro default character set utf8 default collate utf8_general_ci;
+use cadastro;
+CREATE TABLE tabela (
+id int not null auto_increment,
+nome varchar(50) not null,
+nascimento date, 
+sexo enum("F", "M"),
+peso decimal(5,3),
+altura decimal(3,2),
+nacionalidade varchar(20) default "Brasileiro",
+primary key(id)
+) default charset = utf8;
+
+create database clientes02 default character set utf8 default collate utf8_general_ci; 
+create table tabela_cliente(
+id int not null auto_increment,
+nome varchar(30) not null,
+peso decimal(5,2),
+nascimento date,
+velho enum("Y","N"),
+
+ default charset utf8;
