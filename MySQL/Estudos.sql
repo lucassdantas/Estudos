a2b3a2b449961077389c5221b3541914829b853e
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-17 06:44:21 -0300

    alterar dados
    Alter Table nome Add Column (column é opcional) nome tipo 
    se quiser por depois de uma coluna especifica: 
    add (column) nome after nome; 
    colocando "first" vai para a primeira posição 
    alter table nome add nome varchar(30) FIRST;

--- MySQL/Estudos.sql
+++ MySQL/Estudos.sql
@@ -15,3 +15,15 @@ insert into funcionarios
 values
 (default, 'lucas', '2002-01-07', 'web developer', '217');
 
+describe pessoas; 
+desc pessoas; 
+use click;
+alter table funcionarios
+ add column profissao varchar(10);
+ 
+ alter table funcionarios 
+ drop column profissao;
+ 
+ alter table funcionarios add column usa_bone enum("S","N") after nome;
+ alter table funcionarios drop usa_bone;
+ alter table funcionarios add usa_bone varchar(30) after funcao;
