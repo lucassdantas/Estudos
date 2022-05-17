2b859c20d97cbe12bcd71eaa1dc9c0f7727c0620
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-17 07:36:28 -0300

    Add files via upload

--- MySQL/Estudos.sql
+++ MySQL/Estudos.sql
@@ -5,3 +5,15 @@ sexo enum("H","M"),
 salario float(7,2) unsigned,
 funcao varchar(30) 
 ) default charset = utf8;
+
+alter table funcionarios add column frase text ;
+
+select * from funcionarios;
+alter table funcionarios drop nome;
+alter table funcionarios add column id int not null first;
+alter table funcionarios add nome varchar(20) after id;
+alter table funcionarios add primary key(id);
+
+INSERT INTO funcionarios (id, NOME, NASCIMENTO, SEXO, SALARIO, FUNCAO, FRASE)
+values 
+("3", "LUCAS", "2002-01-07", "M", "2000", "dev web", "Viva Cristo Rei!!!");
