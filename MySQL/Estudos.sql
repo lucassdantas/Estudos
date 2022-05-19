a3dbe35f6af828ce04f5231a70869e20a138a4cf
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-19 17:59:11 -0300

    more training of select commands

--- MySQL/Estudos.sql
+++ MySQL/Estudos.sql
@@ -12,7 +12,13 @@ order by aluno;
 update turma_1001
 set sexo = "M" where aluno = "Jennifer" limit 1;
 
+select * from turma_1001;
 select * from turma_1001 where numero > 1 order by aluno desc limit 2  ;
+select * from turma_1001 where numero between 1 and 3;
+select aluno, media_nota from turma_1001 where media_nota in (3, 9);
+select * from turma_1001 where media_nota between 4 and 9;
+select * from turma_1001 where numero >= 3;
+select aluno from turma_1001 where media_nota > 5 and media_nota < 10;
 
 delete from turma_1001 where numero = 5;
 update turma_1001 set sexo = "H" where aluno != "J%" limit 3;
