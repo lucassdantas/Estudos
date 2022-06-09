9b97f49c198a9a5dda7ae4ccff8a403bbae5b3eb
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-06-09 07:34:19 -0300

    Add files via upload

---
+++ PHP/resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title')</title>
+        
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        
+    </head>
+    <body class="antialiased">
+        @yield('content')
+        <footer>
+            <p>rodape</p>
+        </footer>
+    </body>
+</html>
---
+++ PHP/resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.main")
+@section("title", "home")
+@section("content")
+    <h1>Titulo</h1>
+    <h2>Subtitulo</h2>
+@endsection
